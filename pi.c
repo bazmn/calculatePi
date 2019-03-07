bcf1f1434a9062357236e57cff43b8ebc00b5a4c
Brenton Schulz <brenton.schulz@newcastle.edu.au>
2019-03-07 01:25:30 +0000

    Added pseudocode

--- pi.c
+++ pi.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 
-main()
+/* Pseudocode:
+BEGIN
+	integer countTotal = 0
+	integer countInside = 0
+	WHILE countTotal < A large number
+		x = random number between 0 and 1
+		y = random number between 0 and 1
+		countTotal = countTotal + 1
+		IF x*x + y*y < 1
+			countInside = countInside + 1
+		ENDIF
+	ENDWHILE
+	pi = 4*countInside/countTotal
+	PRINT pi
+END
+*/
+
+int main()
 {
    // This is blank for now, we will fill it in during the lecture
+   return 0;
 }
